7251709c316ac22c5201dfaf8767bf83afc535d7
alwintje <alwinkroesen@gmail.com>
2017-10-13 17:02:15 +0200

    Core create a board

--- server/core.js
+++ server/core.js
@@ -1,11 +1,18 @@
-exports.Main = function(){
+exports.Main = function(roomId, socket){
 
 
     this.board = new Board();
+    this.socket = socket;
+    this.roomId = roomId;
 
     this.getBoard = function (){
         return this.board;
     };
+    this.createGame = function () {
+        this.board.createBoard();
+        socket.broadcast.to(roomId).emit("updateGame", {board: this.board.getGridData()});
+    };
+
     return this;
 };
 
@@ -20,6 +27,64 @@ function Board(){
     this.addPlayer = function(id, name){
         this.players[id] = new Player(id, name);
     };
+
+    this.createBoard = function(){
+
+
+        // Kijkt of het bord beedte/hoogte even is
+        var evenWidth = this.width % 2 === 0;
+        var evenHeight = this.height % 2 === 0;
+
+        // Het midden van de x- en y-as
+        var middleWidth = this.width/2;
+        var middleHeight = this.height/2;
+
+        for(var y=0; y < this.height ;y++){
+            this.grid[y] = [];
+            for(var x=0; x < this.width ;x++){
+
+                if(y === 0 || x === 0 || y === this.height-1 || x === this.width-1){
+                    this.grid[y][x] = 1;
+                }else{
+
+                    // For an X
+                    //   ( (x+y === this.width-1 || x+y === this.height-1) && x > 1  && y > 1 ) || (x === y && x !== 1 && x !== this.width-2)
+                    //
+
+                    this.grid[y][x] = 0;
+
+                    // Breakable blocks as an X
+                    if( ( (x+y === this.width-1 || x+y === this.height-1) && x > 1  && y > 1 ) || (x === y && x !== 1 && x !== this.width-2) ){
+                        // this.grid[y][x] = 2;
+                    }
+
+                    // Breakable blocks (sides)
+                    if( (x > 4 && x < this.width-5 && ( y === 1 || y === this.height-2 ) ) || // Top and bottom
+                        (y > 4 && y < this.height-5 && ( x === 1 || x === this.width-2 ) ) ){ // Left and right
+                        this.grid[y][x] = 2;
+                    }
+
+
+                    // Breakable blocks (whole board)
+                    if( (x % 3 === ( !evenWidth || x < middleWidth ? 0 : 1 ) && x > 1 && x < this.width-2  ) ||
+                        (y % 3 === ( !evenHeight || y < middleHeight ? 0 : 1 ) && y > 1 && y < this.height-2 )
+                    ){
+                        this.grid[y][x] = 2;
+                    }
+
+                    // Stones in the middle
+                    if( y % 2 === ( !evenHeight || y < middleHeight ? 0 : 1 ) &&
+                        x % 2 === ( !evenWidth || x < middleWidth ? 0 : 1 ) ){
+                        this.grid[y][x] = 1;
+                    }
+                }
+            }
+        }
+    };
+
+    this.getGridData = function(){
+        return this.grid;
+    }
 
 }
 
